6afdf7a81302a8b87eb6cab843323af04c43b578
khaihoan <ngonguyenkhaihoan@gmail.com>
2021-11-30 11:29:40 +0700

    @manytoOne image

--- src/main/java/com/example/loverbe/model/Image.java
+++ src/main/java/com/example/loverbe/model/Image.java
@@ -2,10 +2,7 @@ package com.example.loverbe.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
@@ -14,5 +11,6 @@ public class Image {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @ManyToOne
     private User user;
 }
