d3268d7740aba0973db951230c5b4d3fd9d7eabb
tavanvu <vanvu1998phucduc@gmail.com>
2021-11-30 11:06:15 +0700

    CRUD feedback

---
+++ src/main/java/com/example/loverbe/service/feedback/FeedbackService.java
@@ -0,0 +1,34 @@
+package com.example.loverbe.service.feedback;
+
+import com.example.loverbe.model.Feedback;
+import com.example.loverbe.repository.IFeedbackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class FeedbackService implements IFeedbackService{
+
+    @Autowired
+    private IFeedbackRepository feedbackRepository;
+
+    @Override
+    public Iterable<Feedback> findAll() {
+        return feedbackRepository.findAll();
+    }
+
+    @Override
+    public Feedback save(Feedback feedback) {
+        return feedbackRepository.save(feedback);
+    }
+
+    @Override
+    public void delete(Long id) {
+        feedbackRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Feedback> findById(Long id) {
+        return feedbackRepository.findById(id);
+    }
+}
---
+++ src/main/java/com/example/loverbe/service/feedback/IFeedbackService.java
@@ -0,0 +1,7 @@
+package com.example.loverbe.service.feedback;
+
+import com.example.loverbe.model.Feedback;
+import com.example.loverbe.service.IGeneralService;
+
+public interface IFeedbackService extends IGeneralService<Feedback> {
+}
