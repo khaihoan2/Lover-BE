37e6275b427439a316c465be775007c466636ba5
chinhpc <phancongchinh@gmail.com>
2021-11-30 09:55:27 +0700

    On init

---
+++ src/test/java/com/example/loverbe/LoverBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loverbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoverBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
