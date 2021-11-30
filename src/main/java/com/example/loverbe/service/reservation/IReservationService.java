7b1c68c62a2e070fa7aef28916487e82c7ca685e
ThanhTran <tranvanthanhtran.tt@gmail.com>
2021-11-30 10:18:43 +0700

    add Reservation, IReservationRepository, ReservationService

---
+++ src/main/java/com/example/loverbe/service/reservation/IReservationService.java
@@ -0,0 +1,7 @@
+package com.example.loverbe.service.reservation;
+
+import com.example.loverbe.model.Reservation;
+import com.example.loverbe.service.IGeneralService;
+
+public interface IReservationService extends IGeneralService<Reservation> {
+}
