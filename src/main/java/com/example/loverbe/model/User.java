b88942dadf9141359f32e3d692aff0e0e21093c9
ThanhTran <tranvanthanhtran.tt@gmail.com>
2021-11-30 12:14:54 +0700

    add JwtResponse

--- src/main/java/com/example/loverbe/model/User.java
+++ src/main/java/com/example/loverbe/model/User.java
@@ -32,15 +32,15 @@ public class User {
     @ManyToOne
     private Nationality nationality;
 
-    private double height;
+    private Double height;
 
-    private double weight;
+    private Double weight;
 
-    private double bust;
+    private Double bust;
 
-    private double waist;
+    private Double waist;
 
-    private double hips;
+    private Double hips;
 
     private String description;
 
@@ -54,9 +54,9 @@ public class User {
 
     private String password;
 
-    private int viewCounter;
+    private Integer viewCounter;
 
-    private int rentedCounter;
+    private Integer rentedCounter;
 
     private String status;
 }
