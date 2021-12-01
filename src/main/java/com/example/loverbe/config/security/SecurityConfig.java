d1a740e7d5765f5770ee5e982b9fa6287d588726
ThanhTran <tranvanthanhtran.tt@gmail.com>
2021-12-01 13:24:25 +0700

    disable security in security config

--- src/main/java/com/example/loverbe/config/security/SecurityConfig.java
+++ src/main/java/com/example/loverbe/config/security/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().ignoringAntMatchers("/**");
         http.authorizeRequests()
                 .antMatchers("/", "/api/auth/login", "/api/auth/register").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
                 .and().csrf().disable();
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         http.cors();
