f2c71f8f191350b7e32dbf719d75fe70789031d0
ThanhTran <tranvanthanhtran.tt@gmail.com>
2021-12-02 16:35:59 +0700

    them /api/users/checkUsername/** permitAll

--- src/main/java/com/example/loverbe/config/security/SecurityConfig.java
+++ src/main/java/com/example/loverbe/config/security/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().ignoringAntMatchers("/**");
         http.authorizeRequests()
-                .antMatchers("/", "/api/auth/login", "/api/auth/register").permitAll()
+                .antMatchers("/", "/api/users/checkUsername/**", "/api/auth/login", "/api/auth/register").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable();
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
